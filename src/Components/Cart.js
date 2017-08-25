2da23761c8dcb9891086b88c95e5a65e1a623cdf
NusratAra <mubin.ara.24@gmail.com>
2017-08-26 03:05:09 +0600

    cart

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -2,11 +2,46 @@ import React from 'react';
 import SearchResult from './SearchResult.js';
 
 class Cart extends React.Component {
+  constructor(){
+    super();
+    this.renderCart = this.renderCart.bind(this);
+  }
+
+  renderCart(key){
+    const items = this.props.itemList[key];
+    const count = this.props.cartList[key];
+
+    return(
+      <li key={key}>
+        <p>{count} {items._source.title}</p>
+        <p>{count} ${ count * items._source.price}</p>
+      </li>
+    )
+  }
+
+
+
   render() {
+    const cartID = Object.keys(this.props.cartList);
+    const total = cartID.reduce((prevTotal, key) => {
+      const items = this.props.itemList[key];
+      const count = this.props.cart[key];
+      const isAvailable = items && items.status==='available';
+      if(isAvailable){
+        return prevTotal + (count * items.price || 0)
+      }
+      return prevTotal;
+    }, 0);
+
+
+
+
     return (
       <div>
         <p>Cart</p>
-      </div>
+        <p>{cartID.map(this.renderCart)}</p>
+        {total}
+        </div>
     );
   }
 }
--- src/Components/SearchResult.js
+++ src/Components/SearchResult.js
@@ -17,20 +17,20 @@ addCart(item, key) {
     const itemList = {...this.state.itemList};
     const cartList = {...this.state.cartList};
     cartList[key] = cartList[key] + 1 || 1;
-    itemList[key] = itemList;
+    itemList[key] = item;
     this.setState({ cartList });
     this.setState({ itemList });
 }
 
 
   render() {
-    const productList = this.props.products.map((products, index) => (
-      <li key={index}>
+    const productList = this.props.products.map((products, ind) => (
+      <li key={ind}>
       <h5 >{products._source.title}</h5>
       <p>${products._source.price}</p>
       <img className="menu-fish"
       src={products._source.images} alt={products._source.title} />
-        <button key={index} onClick={() => this.addCart(products, products._source.title)}> + Cart</button>
+        <button key={ind} onClick={() => this.addCart(products, products._source.title)}> + Cart</button>
       </li>
     ));
 
@@ -41,6 +41,8 @@ addCart(item, key) {
         <ul>
         {productList}
         </ul>
+        <Cart itemList={this.state.itemList} cartList={this.state.cartList} />
+
       </div>
     );
   }
