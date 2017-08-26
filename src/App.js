fe184e4738faee290c7bf8a88aed37b351ea170e
NusratAra <mubin.ara.24@gmail.com>
2017-08-26 12:44:45 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -2,8 +2,28 @@ import React from 'react';
 import Cart from './Components/Cart.js';
 import Search from './Components/Search.js';
 import Data from './Components/Data.js';
+import SearchResult from './Components/SearchResult.js';
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.addToOrder = this.addToOrder.bind(this);
+
+    // getinitialState
+    this.state = {
+      order: {}
+    };
+  }
+
+  addToOrder(key) {
+  // take a copy of our state
+  const order = {...this.state.order};
+  // update or add the new number of fish ordered
+  order[key] = order[key] + 1 || 1;
+  // update our state
+  this.setState({ order });
+}
 
 
 
@@ -14,7 +34,22 @@ class App extends React.Component {
           <div className="menu">
             <Search />
           </div>
-          <Cart /  >
+
+
+          <ul>
+            {
+              Object
+                .keys(this.state.order)
+                .map(key => <SearchResult key={key} index={key}  addCart={this.addToOrder}/>)
+            }
+          </ul>
+
+
+
+
+
+
+          <Cart order={this.state.order}/>
         </div>
     )
   }
